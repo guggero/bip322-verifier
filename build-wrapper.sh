654729a85aae0024aa2c91ba26b4590ae3745506
Oli <gugger@gmail.com>
2026-04-10 09:13:06 +0200

    Add benchmark, make WASM reproducible

--- build-wrapper.sh
+++ build-wrapper.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" .
 
 GOOS=js GOARCH=wasm go build \
-  -ldflags="-s -w" \
+  -ldflags="-s -w -buildid=" \
   -trimpath \
+  -buildvcs=false \
   -o bip322.wasm .
